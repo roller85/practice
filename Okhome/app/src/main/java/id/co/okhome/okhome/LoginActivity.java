01bce7a7bd04d5c435144c618cc26ffc280ac865
Daehyun <rollercoaster85@gmail.com>
2016-03-08 22:27:14 +0900

    initial commit

---
+++ Okhome/app/src/main/java/id/co/okhome/okhome/LoginActivity.java
@@ -0,0 +1,145 @@
+package id.co.okhome.okhome;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import id.co.okhome.okhome.Server.ServerAPI;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public final static String EXTRA_MESSAGE = "id.co.okhome.loginapp2.MESSAGE";
+    private EditText appEmail, appPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        appEmail = (EditText) findViewById(R.id.app_email);
+        appPassword = (EditText) findViewById(R.id.app_pw);
+
+        findViewById(R.id.btn_login).setOnClickListener(this);
+        findViewById(R.id.btn_signup).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login:
+                attemptLogin();
+                break;
+            case R.id.btn_signup:
+                Intent intent = new Intent(this, SignupActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    private void attemptLogin() {
+        String email = appEmail.getText().toString();
+        String password = appPassword.getText().toString();
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            LoginApiTask loginApiTask = new LoginApiTask();
+            loginApiTask.execute(email, password);
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "No Connection", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class LoginApiTask extends AsyncTask<String, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            String id = params[0];
+            String password = params[1];
+
+            try {
+
+                URL url = new URL(ServerAPI.LOGIN + "?id=" + id + "&password=" + password);
+
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                BufferedInputStream in = new BufferedInputStream((urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+
+                byte[] buffer = new byte[8];
+                int i;
+                while ((i = in.read(buffer)) != -1) {
+                    stringBuilder.append(new String(buffer, 0, i));
+                }
+
+                urlConnection.disconnect();
+
+                String result = stringBuilder.toString();
+
+                if (result.equals("success")) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+
+            } catch (MalformedURLException e) {
+                Log.d("LoginApiTask", e.getMessage());
+                return false;
+            } catch (IOException e) {
+                Log.d("LoginApiTask", e.getMessage());
+                return false;
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            if (aBoolean) {
+                Toast.makeText(getApplicationContext(), "success", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                String message = appEmail.getText().toString();
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+                finish();
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_SHORT).show();
+            }
+            super.onPostExecute(aBoolean);
+        }
+    }
+}
---
+++ Okhome/app/src/main/java/id/co/okhome/okhome/Server/ServerAPI.java
@@ -0,0 +1,8 @@
+package id.co.okhome.okhome.Server;
+
+public class ServerAPI{
+
+    public static final String BASE_URL = "http://ec2-52-74-249-70.ap-southeast-1.compute.amazonaws.com";
+    public static final String LOGIN = BASE_URL + "/login.php";
+    public static final String SIGNUP = BASE_URL + "/signup.php";
+}
